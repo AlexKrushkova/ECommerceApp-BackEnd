2c7bf50cc2b28898f24df13d66319bfe99a95c9e
alexkrushkova <alexkrushkova@gmail.com>
2021-12-06 09:19:36 +0100

    db created and connected

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
-
 const app =express();
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb+srv://alexkrushkova:ssaassaa@cluster0.dvcb6.mongodb.net/shop?retryWrites=true&w=majority"
+).then(() => console.log("DB connection successful!!"))
+  .catch((err)=> {
+    console.log(err);
+})
 
 app.listen(5000, () => {
   console.log("Backend server is running!")
