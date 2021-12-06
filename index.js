bdaba49ae75c6db444128915e5f0f2e07439feb8
alexkrushkova <alexkrushkova@gmail.com>
2021-12-06 09:22:38 +0100

    security issues fixed and dotenv implemented

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app =express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 mongoose
-  .connect("mongodb+srv://alexkrushkova:ssaassaa@cluster0.dvcb6.mongodb.net/shop?retryWrites=true&w=majority"
+  .connect(
+    process.env.MONGO_URL
 ).then(() => console.log("DB connection successful!!"))
   .catch((err)=> {
     console.log(err);
