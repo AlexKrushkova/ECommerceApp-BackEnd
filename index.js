220e92f6ef1c554fb3d9b048de339822038601af
alexkrushkova <alexkrushkova@gmail.com>
2021-12-07 20:23:40 +0100

    routers bugs fixed

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const userRoute =  require("../api/routes/users");
 const authRoute =  require("../api/routes/auth");
 const productRoute =  require("../api/routes/product");
+const cartRoute =  require("../api/routes/cart");
+const orderRoute =  require("../api/routes/order");
 
 dotenv.config();
 
@@ -21,6 +23,8 @@ app.use(express.json());
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders", orderRoute);
 
 
 app.listen(process.env.PORT || 5000, () => {
--- routes/order.js
+++ routes/order.js
@@ -5,11 +5,11 @@ const Order = require("../models/Order");
 
 //CREATE
 router.post("/", verifyToken, async (req, res) => {
-  const newCart = new Cart(req.body)
+  const newOrder = new Order(req.body)
 
   try {
-    const savedCart = await newCart.save();
-    res.status(200).json(savedCart);
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
   }
   catch (err) {
     res.status(500).json(err);
@@ -17,14 +17,14 @@ router.post("/", verifyToken, async (req, res) => {
 })
 
 //UPDATE
-router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
 
   try {
-    const updatedCart = await Cart.findByIdAndUpdate(req.params.id, {
+    const updatedOrder = await Order.findByIdAndUpdate(req.params.id, {
       $set: req.body
     }, { new: true }
     );
-    res.status(200).json(updatedCart);
+    res.status(200).json(updatedOrder);
   }
   catch (err) {
     res.status(500).json(err);
@@ -33,35 +33,63 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
 
 
 //DELETE
-router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
   try {
-    await Cart.findByIdAndDelete(req.param.id)
-    res.status(200).json("Cart has been deleted...")
+    await Order.findByIdAndDelete(req.param.id)
+    res.status(200).json("Order has been deleted...")
   }
   catch (err) {
     res.status(500).json(err);
   }
 })
 
-//GET USER CART
+//GET USER ORDERS
 router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
   try {
-    const cart = await Cart.findOne({userId: req.params.userId})
-    res.status(200).json(cart)
+    const orders = await Order.find({ userId: req.params.userId })
+    res.status(200).json(orders)
   }
   catch (err) {
     res.status(500).json(err);
   }
 })
 
-//GET ALL 
-get("/", verifyTokenAndAdmin, async (req,res)=>{
+//GET ALL ORDERS
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
 
-  try{
-    const carts = await Cart.find();
-    res.status(200).json(carts);
+  try {
+    const orders = await Order.find();
+    res.status(200).json(orders);
   }
-  catch(err){
+  catch (err) {
+    res.status(500).json(err);
+  }
+})
+
+//GET MONTHLY INCOME
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const date = new Date();
+  const lastMonth = new Date(date.setMonth(date.getMonth() - 1))
+  const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1))
+
+  try {
+    const income = await Order.aggregate([
+      { $match: { createdAt: { $gte: previousMonth } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        }
+      }
+    ])
+      res.status(200).json(income);
+  }
+  catch (err) {
     res.status(500).json(err);
   }
 })
